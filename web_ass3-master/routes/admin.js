c33dbed03c2ac1494489365d576b8cc603e36248
TemirlanBakytbekov <123005454+TemirlanBakytbekov@users.noreply.github.com>
2024-02-13 03:28:47 +0300

    Update admin.js

--- web_ass3-master/routes/admin.js
+++ web_ass3-master/routes/admin.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 const { MongoClient } = require('mongodb');
 const { ObjectId } = require('mongodb');
 
-const uri = 'mongodb+srv://mustiksarva:table1234554321@mailing.qungyk9.mongodb.net/?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://temik20172:<U4f88tem1k!>@cluster0.gu7rxob.mongodb.net/';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect();
